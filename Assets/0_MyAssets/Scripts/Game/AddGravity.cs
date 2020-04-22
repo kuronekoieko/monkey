3b119f5a2265bff57595ebb531b413a45403bc94
家子智成 <>
2020-04-23 00:13:10 +0900

    振り子作成

---
+++ Assets/0_MyAssets/Scripts/Game/AddGravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddGravity : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        rb.AddForce(Vector3.down * 100, ForceMode.Acceleration);
+
+    }
+}
---
+++ Assets/0_MyAssets/Scripts/Game/Rope.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+
+    /*
+    【Unity】なわとびの縄のシミュレーション
+    http://nn-hokuson.hatenablog.com/entry/2018/01/30/200050
+    */
+
+    LineRenderer line;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.material = new Material(Shader.Find("Unlit/Color"));
+        line.positionCount = transform.childCount;
+
+        foreach (Transform v in transform)
+        {
+            v.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        int idx = 0;
+        foreach (Transform v in transform)
+        {
+            line.SetPosition(idx, v.position);
+            idx++;
+        }
+    }
+}
